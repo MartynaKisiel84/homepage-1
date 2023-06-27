6f18f7a692768d7c058200f3786dfa41fc17e658
MartynaKisiel84 <128509890+MartynaKisiel84@users.noreply.github.com>
2023-06-27 23:50:44 +0200

    remove 1 line code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 {
     const welcome = () => {
-     console.log("Witam wszystkich serdecznie")
+        console.log("Witam wszystkich serdecznie")
     }
-    
+
     const toggleBackground = () => {
-         const bodyElement = document.querySelector(".js-body");
-         const colorElement = document.querySelector(".js-color");
+        const bodyElement = document.querySelector(".js-body");
+        const colorElement = document.querySelector(".js-color");
 
         bodyElement.classList.toggle("body--dark");
         colorElement.innerText = bodyElement.classList.contains("body--dark") ? "jasny" : "ciemny";
@@ -14,13 +14,13 @@
     const init = () => {
         const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
         changeBackgroundButton.addEventListener("click", toggleBackground);
-        
-       welcome();
+
+        welcome();
     };
-    
+
     init();
-    
-    }
-    
+
+}
+
 
 
